3fae7195f8904eb41396aa51581ad39482f097db
Alexandr Nabokin <sashkai@ukr.net>
2019-12-25 18:14:51 +0200

    webpack config fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
+// для того, чтобы прописать конфиг аутпут (результат работы вебпака) и сложть в определенном месте
 const path = require('path');
+//Забрал из библиотеки вебпак сам вебпак
 const webpack = require('webpack');
+// кладет преобразоанные из сассов цсс в нужное место(финальный цсс)
 const ExtractCssChunks = require("extract-css-chunks-webpack-plugin");
+// после отработки вебпака создается новая папка билд/дист. Чтобы она самоудалялась пред тем, как повится новая при след
+// обработке вебпаком, нужен этот плагин
 const CleanWebpackPlugin = require('clean-webpack-plugin'); 
+// берет хтмл и кладет в нужную папку
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let pathsToClean = ['dist', 'build']
 
 let cleanOptions = {
     root: process.cwd(),
-    exclude: ['shared.js'],
     verbose: true,
     dry: false
 }
 
 const config = {
     target: "web",
+    // файлы. из которых создается финальные файлы, преобразованные вебпаком
     entry: {
         "main": ["./src/main.js", "./src/assets/main.scss"]
     }, 
     resolve: {
-        extensions: [".tsx", ".js", ".scss", ".css", ".json"],
-        alias: {
-            jquery: "jquery/src/jquery"
-        }
+        extensions: [".js", ".scss", ".css"]
     },
+    // набор правил, по которым будут резолвиться файлы жс, цсс и картинки и т д
     module: {
         rules: [
             {
+                // преобразование джс
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
@@ -37,16 +42,19 @@ const config = {
                 }
             },
             {
+                // преобразование цсс
                 test: /\.(sass|scss)$/,
                 use: [ExtractCssChunks.loader, "css-loader", "postcss-loader", "sass-loader"],
             },
             {
+                // проверка ошибок. При запуске вебпака в терминале отобразятся ошибки и меат, где они допущені
                 test: /\.js$/,
                 enforce: "pre",
                 exclude: /node_modules/,
                 loader: "eslint-loader"
             },
             {
+                // для переноса картинок в дист/билд и сжатия по лимиту
                 test: /\.(jpg|png|mp4)$/,
                 loader: 'url-loader',
                 options: {
@@ -81,17 +89,14 @@ const config = {
             }
         ),
         new CleanWebpackPlugin(pathsToClean,  cleanOptions),
+        // При отработке вебпака все урлі в картинках, видео и тд заменится на срц, 
+        // т.к. папка будет новая где они будут лежать
         new webpack.LoaderOptionsPlugin({
             options: {
                 customInterpolateName: (url) => {
                     return url.replace(/src/g, '');
                 }
             }
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.$': 'jquery'
         })
     ],
 
@@ -102,17 +107,18 @@ const config = {
             chunks: 'all'
         }
     },
-    
+    // вебпак отрабатвает при каждом сохранении кода
     devServer: {
         compress: true,
         open: true,
         inline: true,
         stats: 'errors-only',
         watchContentBase: true,
-        disableHostCheck: true
+        contentBase: path.join(__dirname, '/src'),
+        port: 3000
     }  
 };
-
+// Функция выбора дев или прод режима
 module.exports = (env, argv) => {
 
     if (argv.mode === 'development') {
