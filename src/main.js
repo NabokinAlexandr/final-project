c307cc3be4a63de6084852c42282e57b60901273
Alexandr Nabokin <sashkai@ukr.net>
2019-12-27 22:19:02 +0200

    added routing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {User} from './js/user.js';
 import {Validator} from './js/validator.js';
-import {Apis} from './api/api.js';
+import {Apis} from './js/api/api.js';
+import './js/router/router-initiation.js';
 (function() {
     
 }());
